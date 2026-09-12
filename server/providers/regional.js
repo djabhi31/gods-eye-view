318d37ad8d8a39de9e1bd02259a78547d8283abd
Sameh Khamis <sameh@halfpixel.ai>
2026-09-12 12:59:39 -0700

    refactor: separate regional briefing and weather sources

---
+++ server/providers/regional.js
@@ -0,0 +1,3 @@
+export { regionalBriefProxy, regionalBriefHasAnySource } from './regional/briefing.js';
+export { weatherEffectsProxy } from './regional/weather-effects.js';
+export { validRegionalPoint } from './regional/query.js';
